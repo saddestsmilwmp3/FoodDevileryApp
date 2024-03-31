05aae14166b807a61e91be637824cde5046e883d
Maria <fahurdinovamaria@gmail.com>
2024-03-31 21:47:29 +0300

    Created base project and removed storyboard references

---
+++ FoofDevileryAppMasha/FoofDevileryAppMasha/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoofDevileryAppMasha
+//
+//  Created by Мария on 31.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
